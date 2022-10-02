daec67c9075ded75f3db52432db610defd541364
AshredCR <55962947+EdCastillo@users.noreply.github.com>
2022-10-02 14:09:50 -0600

    Agregar archivos de proyecto.

---
+++ WebAPI/Helpers/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+        public string MinutosExpiracionToken { get; set; }
+        public string BaseDatosSeguridad { get; set; }
+
+    }
+}
---
+++ WebAPI/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿namespace WebAPI.Models
+{
+    public class UserModel
+    {
+        public int Codigo { get; set; }
+
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string CorreoElectronico { get; set; }
+
+        public string NumeroTelefono { get; set; }
+
+        public string Username { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+        public string Estado { get; set; }
+    }
+}
